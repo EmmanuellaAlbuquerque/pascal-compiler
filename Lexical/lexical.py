7bc0e0e3be9e70319cf68753c78717e11d325b8d
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2021-03-22 14:19:35 -0300

    fix: the name of the tokens types, add RW

--- Lexical/lexical.py
+++ Lexical/lexical.py
@@ -15,35 +15,40 @@ def addToken(word, sym, line_counter):
     output_table['Line'] = line_counter
     output_list.append(output_table.copy())
 
+
 def fill_blanks(data, size):
     data = str(data)
-    if len(data)>size:
+    if len(data) > size:
         data = data[:size]
-    return str('|  ' + data.ljust(size) )
+    return str('|  ' + data.ljust(size))
+
 
 def printTable(table):
-    table.append(fill_blanks('Token', 30) + fill_blanks("Symbol", 30) + fill_blanks('Line', 5) + '  |')
-    table.append('================================================================================')
+    table.append(fill_blanks('Token', 30) + fill_blanks("Symbol",
+                                                        30) + fill_blanks('Line', 5) + '  |')
+    table.append(
+        '================================================================================')
     for item in output_list:
         token = item["Token"].rstrip("\n")
         symbol = item["Symbol"].rstrip("\n")
         line = item["Line"]
-        table.append(fill_blanks(token, 30) + fill_blanks(symbol, 30) +  fill_blanks(line, 5) + '  |')
+        table.append(fill_blanks(token, 30) + fill_blanks(symbol,
+                                                          30) + fill_blanks(line, 5) + '  |')
 
     with open("./resultado/tabela.txt", "w") as file:
         for i in table:
             print(i)
             file.write(str(i + '\n'))
 
-   
+
 def switchReservedSymbol():
     for item in output_list:
         if(item['Token'] in reserved_words):
-            item['Symbol'] = 'reserved_word'
+            item['Symbol'] = 'Reserved Word'
 
 
 reserved_words = ('program', 'var', 'begin', 'end', 'integer', 'real', 'if', 'then',
-                  'else')
+                  'else', 'boolean', 'procedure', 'while', 'do', 'not')
 
 file_src = os.path.join("../.pas", "test_comments.pas")
 
@@ -61,13 +66,7 @@ tabela = []
 
 while(True):
     current_char += 1
-    # if (char_list[current_char] == ' '):
-    #     # continue
-    #     exit()
-    # print(char_list[current_char])
-
     if (current_char == len(char_list)):
-        # print(line_counter)
         switchReservedSymbol()
         printTable(tabela)
         exit()
@@ -87,25 +86,24 @@ while(True):
             token = char_list[current_char]
             current_state = 3
         elif (char_list[current_char] == '.'):
-            # token = char_list[current_char]
-            print('delimiter: ' + token)
-            addToken(char_list[current_char], 'delimiter', line_counter)
-        elif (char_list[current_char] == '\n'):
+            # print('delimiter: ' + token)
+            addToken(char_list[current_char], 'Delimiter', line_counter)
+        elif (char_list[current_char] == '\n' or char_list[current_char] == '\t'):
             line_counter += 1
         elif (char_list[current_char] == '{'):
             token = char_list[current_char]
             current_state = 6
         elif (char_list[current_char] == ';'):
-            addToken(char_list[current_char], 'ponto;', line_counter)
+            addToken(char_list[current_char], 'Delimiter', line_counter)
         continue
-    if (current_state == 1):  # Q1
+    if (current_state == 1):
         az09 = re.findall("\w", char_list[current_char])
         if (len(az09) > 0):
             current_state = 1
             token += char_list[current_char]
         else:
-            print('identifier: ' + token)
-            addToken(token, 'identifier', line_counter)
+            # print('identifier: ' + token)
+            addToken(token, 'Identifier', line_counter)
             current_char -= 1
             current_state = 0
             token = ''
@@ -117,8 +115,8 @@ while(True):
             current_state = 2
             token += char_list[current_char]
         else:
-            print('number: ' + token)
-            addToken(token, 'number', line_counter)
+            # print('number: ' + token)
+            addToken(token, 'Number', line_counter)
             current_char -= 1
             current_state = 0
             token = ''
@@ -128,28 +126,24 @@ while(True):
             current_state = 4
             token += char_list[current_char]
         else:
-            print('delimiter: ' + token)
-            addToken(token, 'delimiter', line_counter)
+            # print('delimiter: ' + token)
+            addToken(token, 'Delimiter', line_counter)
             current_state = 0
             token = ''
         continue
     if (current_state == 4):
-        print('assignment command: ' + token)
-        addToken(token, 'assignment command', line_counter)
+        # print('assignment command: ' + token)
+        addToken(token, 'Assignment Command', line_counter)
         current_state = 0
         token = ''
         continue
     if (current_state == 6):
         token += char_list[current_char]
         if(char_list[current_char] == '}'):
-            print('comments: ' + token)
-            addToken(token, 'comments', line_counter)
+            # print('comments: ' + token)
+            addToken(token, 'Comments', line_counter)
             current_state = 0
             token = ''
         elif (char_list[current_char] == '\n'):
             line_counter += 1
         continue
-
-# def previous_character():
-# def posterior_character():
-# identifier
